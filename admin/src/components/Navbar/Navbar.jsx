823d6e3c4199e57770b5fadfe34fb924244404ac
adhithyashine <adhithyashine@gmail.com>
2025-01-20 12:23:08 +0530

    admin panel ready

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
-      <img className='profile' src={assets.profile_image} alt="" />
+      <img className='logo' src={assets.logo} alt="Logo" />
+      <h1 className='navbar-heading'>Organic Guru Admin Panel</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
